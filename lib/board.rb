a32d15adcd82d4fc948f3430c7dc332beffac984
Kerwin McMillian <marink128@gmail.com>
2020-05-02 03:04:59 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -3,4 +3,12 @@ board = "Board"
 
 board = ["", "", "", "", "", "", "", "", ""]
 
-board[0]
+board[0] = ""
+board[1] = ""
+board[2] = ""
+board[3] = ""
+board[4] = ""
+board[5] = ""
+board[6] = ""
+board[7] = ""
+board[8] = ""
